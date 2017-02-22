c36e83f48be66b7d85d6a280dbb8a2cb51c4392e
Jakob John List Tøth <jakob2botg@gmail.com>
2017-02-22 14:05:36 +0100

    Fixed Dependancies

--- MinioningServer/ClientConnection/src/main/java/minioning/clientconnection/Installer.java
+++ MinioningServer/ClientConnection/src/main/java/minioning/clientconnection/Installer.java
@@ -4,11 +4,24 @@
  * and open the template in the editor.
  */
 package minioning.clientconnection;
-
+import org.openide.modules.ModuleInstall;
 /**
  *
  * @author Jakob
  */
-public class Installer {
+public class Installer extends ModuleInstall {
     
+    @Override
+    public void restored() {
+        
+        
+//        ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+//        executor.scheduleAtFixedRate(gameServer.run(), initialDelay, period, TimeUnit.MILLISECONDS);
+    }
+    
+    @Override
+    public void uninstalled() {
+        super.uninstalled(); //To change body of generated methods, choose Tools | Templates.
+
+    }
 }
